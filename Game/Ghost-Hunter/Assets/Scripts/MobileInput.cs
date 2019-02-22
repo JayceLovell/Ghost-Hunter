05721510cea741a455ef8d921adcef50ebfb6755
Jayce <jaycedlovell@gmail.com>
2019-02-22 11:09:56 -0500

    resolve #30

--- Game/Ghost-Hunter/Assets/Scripts/MobileInput.cs
+++ Game/Ghost-Hunter/Assets/Scripts/MobileInput.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testing : MonoBehaviour
+public class MobileInput : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,10 @@ public class testing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            // this code is to go to a scene
+            //Application.LoadLevel("YourPreviousLevel");
+            Application.Quit();
+        }
     }
 }
