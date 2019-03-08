55f4f11116179f8e821763dbf37dd261d538b2d5
Jayce <jaycedlovell@gmail.com>
2019-03-08 15:32:41 -0500

    :exclamation:  YES YES I HAVE FOUND THE FORMULA :exclamation:

--- Game/Ghost-Hunter/Assets/Scripts/UpdatePlayerLocation.cs
+++ Game/Ghost-Hunter/Assets/Scripts/UpdatePlayerLocation.cs
@@ -8,7 +8,8 @@ public class UpdatePlayerLocation : MonoBehaviour
 
     public float TestLongitude = 0;
     public float TestLatitude = 0;
-    public GameObject Map;
+    public Vector3 testpos;
+   // public GameObject Map;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +21,13 @@ public class UpdatePlayerLocation : MonoBehaviour
     {
         if(TestLatitude != 0 && TestLongitude != 0)
         {
-            transform.position = new Vector3(TestLongitude,0, TestLatitude);
+            //TO DO : Scaling
+            float latScale = 287 / 180;  //latitude goes from 0(Npole) to 180(Spole)
+            float longiScale = 720 / 360; //longitude goes from 0 to 360 (Greenwich)
+            testpos = new Vector3((TestLatitude * latScale), 0, (TestLongitude * longiScale));
+            transform.position = new Vector3((TestLatitude * latScale), 0, (TestLongitude * longiScale));
+            //transform.position = Quaternion.AngleAxis(TestLongitude, Vector3.up) * Quaternion.AngleAxis(TestLatitude, Vector3.right) * new Vector3(0, 0, 1);
+            //transform.position = new Vector3(TestLongitude,0, TestLatitude);
         }
         else
         {
