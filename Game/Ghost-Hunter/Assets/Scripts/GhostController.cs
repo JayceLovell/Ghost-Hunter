4251c5982e421312d73036ec95db6ecffd1155af
Sargis <sargis@live.ca>
2019-03-14 21:06:51 -0400

    UI Text
    #14

--- Game/Ghost-Hunter/Assets/Scripts/GhostController.cs
+++ Game/Ghost-Hunter/Assets/Scripts/GhostController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GhostController : MonoBehaviour {
 
+    private int count = 3;
+    private float speed;
+    private bool ghostCaught = false;
+    private bool ghostMove = false;
+    public Text countText;
+    public Text winText;
     public Vector3 wayPoint = new Vector3(0, 0, 0);
     public int difficulty = 1;
-    float speed;
 
     // Use this for initialization
     void Start () {
@@ -21,20 +27,39 @@ public class GhostController : MonoBehaviour {
             case 3:
                 speed = 10;
                 break;
+            case 4:
+                speed = 15;
+                break;
         }
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Vector3.Distance(transform.position, wayPoint) > 0.1f )
-        {
-            transform.position = Vector3.MoveTowards(transform.position, wayPoint, speed * Time.deltaTime);
+        countText.text = count.ToString();
+
+        while (count > 0) {
+            count--;
         }
-        else
+
+        if (count <= 0)
         {
-            wayPoint = new Vector3(Random.Range(-5, 5), Random.Range(-3, 3), 0);
+            ghostMove = true;
+        }
+
+        if (ghostMove == true)
+        {
+            if (Vector3.Distance(transform.position, wayPoint) > 0.1f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, wayPoint, speed * Time.deltaTime);
+            }
+            else
+            {
+                wayPoint = new Vector3(Random.Range(-5, 5), Random.Range(-3, 3), 0);
+            }
         }
 
     }
+
+
 }
