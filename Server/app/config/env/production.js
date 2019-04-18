94b493410a879378ffcd96a0b6dd71fb286fb5b3
Daniel Stokes <danielphilipstokes@gmail.com>
2019-04-17 20:02:56 -0400

    Fixed envrionment and config

---
+++ Server/app/config/env/production.js
@@ -0,0 +1,5 @@
+// Set the 'production' environment configuration object
+module.exports = {
+	db: 'mongodb://server:password1@ds155414.mlab.com:55414/heroku_xr6lqs0f',
+	sessionSecret: 'devseshsecret'
+};
